63604b37fd13c69d661b7bb735d8b3feee6d7877
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-06-10 02:49:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter((driver) => driver.toLowerCase() === query.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) => driver.toLowerCase().indexOf(letters.toLowerCase()) === 0);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+}
+
